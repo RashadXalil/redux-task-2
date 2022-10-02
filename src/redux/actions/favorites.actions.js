9e32560c09eb115bdc73ed452a08d26db9c3aff9
RashadXalil <rashadmkh@code.edu.az>
2022-10-02 21:42:20 +0400

    inital

---
+++ src/redux/actions/favorites.actions.js
@@ -0,0 +1,7 @@
+export const emptyFavoriteAction = () => {
+  return { type: 'EMPTY_FAVORITE', payload: '' }
+}
+
+export const addToFavoritesAction = (payload) => {
+  return { type: 'ADD_TO_FAVORITE', payload: payload }
+}
---
+++ src/redux/reducers/favorites.reducer.js
@@ -0,0 +1,12 @@
+export const favoriteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TO_FAVORITE':
+      return [...state, action.payload]
+
+    case 'EMPTY_FAVORITE':
+      return []
+
+    default:
+      return state
+  }
+}
