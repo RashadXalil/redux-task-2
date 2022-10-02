9cbbf2b1571c2c5b12b520e01b1e20b3be338a4d
RashadXalil <rashadmkh@code.edu.az>
2022-10-02 22:41:42 +0400

    import fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
-import Divider from '@mui/material/Divider'
-import Drawer from '@mui/material/Drawer'
-import IconButton from '@mui/material/IconButton'
-import List from '@mui/material/List'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemText from '@mui/material/ListItemText'
 import MenuIcon from '@mui/icons-material/Menu'
-import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
+import {
+  Box,
+  Divider,
+  IconButton,
+  List,
+  AppBar,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Toolbar,
+  Button,
+  Typography,
+} from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 
